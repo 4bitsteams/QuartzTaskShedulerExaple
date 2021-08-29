15ee921dcbd6e02ad47764451152f69929c1d809
rubel islam <rubelislam301@gmail.com>
2021-08-29 23:22:27 +0600

    Add a Class MyJob

---
+++ QuartzTaskShedulerExaple/Models/MyJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartzTaskShedulerExaple.Models
+{
+    public class MyJob
+    {
+        public MyJob(Type type,string Expression)
+        {
+            this.Type = type;
+            this.Expression = Expression;
+        }
+
+        public Type Type { get; set; }
+
+        public string Expression { get; set; }
+    }
+}
