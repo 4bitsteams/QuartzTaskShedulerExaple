5fa4850161468c10c1db2828c6d837ef18d2bd79
rubel islam <rubelislam301@gmail.com>
2021-08-29 23:33:29 +0600

    Add Common Class for File Write

---
+++ QuartzTaskShedulerExaple/Models/Common.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartzTaskShedulerExaple.Models
+{
+    public class Common
+    {
+        public static void Logs(string message, string fileName)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "MyLogs");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            path = Path.Combine(path, fileName);
+
+            using FileStream fileStream = new FileStream(path, FileMode.Create);
+            using TextWriter textWriter = new StreamWriter(fileStream);
+            textWriter.WriteLine();
+        }
+    }
+}
